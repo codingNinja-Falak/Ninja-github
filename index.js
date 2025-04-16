80ae6818c71150d08e369d1c583e330ea93ec194
Falak Chandni <falak.chandni09@gmail.com>
2025-04-16 18:54:42 +0530

    share feature

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log("Learning git2");
-console.log("Second line in the index file");
-console.log("Changes for v2");
+// console.log("Second line in the index file");
+// console.log("Changes for v2");
